bda4a43d055d3d9d4195291a3d50f9fdcae1d211
y1141335509 <y1141335509@gmail.com>
2025-05-05 23:25:00 -0700

    Initial commit: Vite + React Chrome Extension

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/manifest.json', dest: '.' },
+        { src: 'public/*.png', dest: '.' }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+  }
+})
